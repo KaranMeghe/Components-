28604d3a495e2e49786aaa964762b9e39c5acbb0
Karan Meghe <karanmeghe87@gmail.com>
2023-02-10 20:52:49 +0530

    Initial Commit

---
+++ comps/src/App.jsx
@@ -0,0 +1,29 @@
+import Button from "./Components/Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>Click Me!</Button>
+      </div>
+
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+
+      <div>
+        <Button>See Deals!</Button>
+      </div>
+
+      <div>
+        <Button>Hide Adds!</Button>
+      </div>
+
+      <div>
+        <Button>Something!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
